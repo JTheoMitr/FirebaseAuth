5789b11d4b546e16047cf69116e3d2bb4f341575
John <jmitropoulos@alumni.berklee.edu>
2022-03-15 19:55:43 -0500

    Initial commit

---
+++ app/src/main/java/android/example/firebaseauth/PhotoSharing.kt
@@ -0,0 +1,11 @@
+package android.example.firebaseauth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhotoSharing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_sharing)
+    }
+}
