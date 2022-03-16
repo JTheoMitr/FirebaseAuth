d6b240927e371d163b25cd17c324418bc755eb3c
John <jmitropoulos@alumni.berklee.edu>
2022-03-15 20:04:03 -0500

    updated signIn code in Main Activity

--- app/src/main/java/android/example/firebaseauth/MainActivity.kt
+++ app/src/main/java/android/example/firebaseauth/MainActivity.kt
@@ -1,11 +1,36 @@
 package android.example.firebaseauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val current_user = auth.currentUser
+        if (current_user != null) {
+            val intent = Intent(this, PhotoSharing::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun SignIn(view : View) {
+        val email = emailText.text.toString()
+        val password = passwordText.text.toString()
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val currentUser = auth.currentUser!!.email.toString()
+                Toast.makeText(this, "Welcome, ${currentUser}", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, PhotoSharing::class.java)
+
+            }
+        }
     }
 }
